b0c6d3173a824cd41b380a04ecbee71ce402c037
libsyfer <libsyfer@gmail.com>
2024-07-31 22:53:30 +0300

    Добавление интерфейса репозитория ссылок.

---
+++ Web/LinkShortener/LinkShortener.Application/Interfaces/ILinkRepository.cs
@@ -0,0 +1,13 @@
+﻿using LinkShortener.Domain.Entities;
+
+namespace LinkShortener.Application.Interfaces
+{
+    public interface ILinkRepository
+    {
+        Task<IEnumerable<Link>> GetAllAsync();
+
+        Task<Link?> GetByIdAsync(string id);
+
+        Task<string> CreateAsync(Link link);
+    }
+}
