5e3ac8688f836d4740beb93fe460d183d524672b
libsyfer <libsyfer@gmail.com>
2024-07-31 23:22:03 +0300

    Добавление метода расширения для регистрации Mediatr.

---
+++ Web/LinkShortener/LinkShortener.Application/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LinkShortener.Application
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddApplicationDependencies(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+            return services;
+        }
+    }
+}
