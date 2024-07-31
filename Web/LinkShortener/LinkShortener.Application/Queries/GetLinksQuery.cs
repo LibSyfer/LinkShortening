74aebcc2801809b4153879ad8b58404edeaab14a
libsyfer <libsyfer@gmail.com>
2024-07-31 23:11:19 +0300

    Добааление запроса получения всех ссылок.

---
+++ Web/LinkShortener/LinkShortener.Application/Queries/GetLinksQuery.cs
@@ -0,0 +1,7 @@
+﻿using LinkShortener.Domain.Entities;
+using MediatR;
+
+namespace LinkShortener.Application.Queries
+{
+    public record GetLinksQuery : IRequest<IEnumerable<Link>>;
+}
---
+++ Web/LinkShortener/LinkShortener.Application/Queries/GetLinksQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using LinkShortener.Application.Interfaces;
+using LinkShortener.Domain.Entities;
+using MediatR;
+
+namespace LinkShortener.Application.Queries
+{
+    public class GetLinksQueryHandler : IRequestHandler<GetLinksQuery, IEnumerable<Link>>
+    {
+        private readonly ILinkRepository _linkRepository;
+
+        public GetLinksQueryHandler(ILinkRepository linkRepository)
+        {
+            _linkRepository = linkRepository;
+        }
+
+        public Task<IEnumerable<Link>> Handle(GetLinksQuery request, CancellationToken cancellationToken)
+        {
+            return _linkRepository.GetAllAsync(cancellationToken);
+        }
+    }
+}
