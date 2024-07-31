d9187858943572a2114b05a06d51326d4b0b7aef
libsyfer <libsyfer@gmail.com>
2024-07-31 22:48:12 +0300

    Создание сущности ссылки.

---
+++ Web/LinkShortener/LinkShortener.Domain/Entities/Link.cs
@@ -0,0 +1,11 @@
+﻿namespace LinkShortener.Domain.Entities
+{
+    public class Link
+    {
+        public string Id { get; set; }
+
+        public required string SourceLink { get; set; }
+
+        public DateTime? ExpireAt { get; set; }
+    }
+}
